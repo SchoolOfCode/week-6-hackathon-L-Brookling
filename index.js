6d8fe56892ea7debfe2f5b9b9dcfa2492291e497
Louis <178508612+L-Brookling@users.noreply.github.com>
2025-01-17 08:51:04 +0000

    Completed test path 7: Checking ticket is lucky

--- index.js
+++ index.js
@@ -2,24 +2,17 @@ export function isLucky(ticket) {
   if (!ticket) {
     return "Ticket not provided";
   }
-
   if (ticket.length === 6) {
-    return true;
+    if (
+      Number(ticket[0]) + Number(ticket[1]) + Number(ticket[2]) ===
+      Number(ticket[3]) + Number(ticket[4]) + Number(ticket[5])
+    ) {
+      return true;
+    }
   }
   return false;
 }
 
 // if (typeof ticket === "number") {
 //   return "Ticket must be of type string";
-// }
-
-// if (ticket.length === 6) {
-//   if (
-//     Number(ticket[0]) + Number(ticket[1]) + Number(ticket[2]) ===
-//     Number(ticket[3]) + Number(ticket[4]) + Number(ticket[5])
-//   ) {
-//     return true;
-//   }
-// }
-// return false;
 // }
--- tests/index.test.js
+++ tests/index.test.js
@@ -4,60 +4,81 @@ import { isLucky } from "..";
 describe("isLucky", () => {
   describe("when ticket is not entered", () => {
     describe("and ticket is an empty string", () => {
+      //ARRANGE
       test("should return error message string", () => {
+        //ACT
         const ticket = "";
         const result = isLucky(ticket);
+        //ASSERT
         expect(result).toStrictEqual("Ticket not provided");
       });
     });
 
     describe("and ticket is null", () => {
+      //ARRANGE
       test("should return error message string", () => {
+        //ACT
         const ticket = null;
         const result = isLucky(ticket);
+        //ASSERT
         expect(result).toStrictEqual("Ticket not provided");
       });
     });
 
     describe("and ticket is undefined", () => {
+      //ARRANGE
       test("should return error message string", () => {
+        //ACT
         const ticket = undefined;
         const result = isLucky(ticket);
+        //ASSERT
         expect(result).toStrictEqual("Ticket not provided");
       });
     });
   });
 
   describe("when a ticket with 7 characters is entered", () => {
+    //ARRANGE
     test("should return false", () => {
+      //ACT
       const ticket = "3211232";
       const result = isLucky(ticket);
+      //ASSERT
       expect(result).toBe(false);
     });
   });
 
   describe("when a ticket with 6 characters is entered", () => {
-    // describe("and the ticket isn't lucky", () => {
-    //   test("should return false", () => {
-    //     const ticket = "123456";
-    //     const result = isLucky(ticket);
-    //     expect(result).toBe(false);
-    //   });
-    // });
+    describe("and the ticket isn't lucky", () => {
+      //ARRANGE
+      test("should return false", () => {
+        //ACT
+        const ticket = "123456";
+        const result = isLucky(ticket);
+        //ASSERT
+        expect(result).toBe(false);
+      });
+    });
 
     describe("and the ticket is lucky", () => {
+      //ARRANGE
       test("should return true", () => {
+        //ACT
         const ticket = "123321";
         const result = isLucky(ticket);
+        //ASSERT
         expect(result).toBe(true);
       });
     });
   });
 
   describe("when a ticket has 5 characters", () => {
+    //ARRANGE
     test("should return false", () => {
+      //ACT
       const ticket = "23454";
       const result = isLucky(ticket);
+      //ASSERT
       expect(result).toBe(false);
     });
   });
